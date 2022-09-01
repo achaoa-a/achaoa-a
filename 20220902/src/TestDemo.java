226932a22a3aa5f3efba3b73142ac95574a1cb14
ymt <zhuangxu@ymt360.com>
2022-09-01 19:25:36 +0800

    第一次提交代码，演示

---
+++ 20220902/src/TestDemo.java
@@ -0,0 +1,10 @@
+/**
+ * @Author 庄续
+ * @Date 2022/9/1 7:23 下午
+ * @Version 1.0
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
